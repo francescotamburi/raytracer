00579a5779c8c2954b39466889c982f47a16765a
francescotamburi <79522605+francescotamburi@users.noreply.github.com>
2024-07-22 16:12:39 +0800

    First render

---
+++ main.py
@@ -0,0 +1,142 @@
+from primitives import *
+import math
+from PIL import Image
+import numpy as np
+
+global objects
+objects = []
+
+class camera(ray):
+	def __init__(self, position, direction, fov, resolution, screen_x):
+		self.point = position
+		self.vector = direction.normalize()
+		self.ray_vector = self.vector
+		self.fov = math.radians(fov)
+		self.resolution = resolution
+		zero = (0,0,0)
+		self.screen = [[zero for i in range(resolution[0])] for i in range(resolution[1])]
+		self.set_pixel_vectors(screen_x, self.vector.cross(screen_x))
+		#print("pixel shifts = ",self.x_shift.components(), ", ", self.y_shift.components())
+	
+	def set_pixel_vectors(self,x_direction,y_direction):
+		##print("x: ",x_direction.normalize().components()," y: ",y_direction.normalize().components())
+		pixel_shift  = math.tan(self.fov/2)/(self.resolution[0]/2)
+		self.x_shift = x_direction.normalize() * pixel_shift	
+		self.y_shift = y_direction.normalize() * pixel_shift
+	
+	def position(self):
+		return self.point
+	
+	def direction(self):
+		return self.vector
+	
+	def cast_rays(self):
+		self.ray_vector += self.x_shift*int(self.resolution[0]/2)
+		self.ray_vector += self.y_shift*int(self.resolution[1]/2)
+		global start
+		start = self.ray_vector.components()
+		new_screen = []
+		for row in self.screen:
+			new_row = []
+			for pixel in row:
+				##print("raycast coordinates", self.ray_vector.components())
+				intersected_triangles = []
+				for obj in objects:
+					for triangle in obj.triangles:
+						if distance := self.intersection(triangle):
+							intersected_triangles.append((triangle, distance))
+				self.ray_vector -= self.x_shift
+				intersected_triangles.sort(key= lambda x:x[1]) #sort by distance
+				#take closest triangle
+				if intersected_triangles:
+					surface = intersected_triangles[0][0]
+					pixel = surface.get_colour()
+				new_row.append(pixel)
+			new_screen.append(new_row)
+			self.ray_vector += self.x_shift*int(self.resolution[0])
+			self.ray_vector += -self.y_shift
+		self.screen = new_screen
+		global end
+		end = self.ray_vector.components()
+		return self.screen
+
+class box:
+	def __init__(self, position, u, v, w):
+		self.position = position
+		self.u = u
+		self.v = v
+		self.w = w
+		self.calculate_triangles()
+	
+	def calculate_triangles(self):
+		A = self.position
+		B = A.translate(self.u)
+		C = B.translate(self.v)
+		D = C.translate(-self.u)
+		E = D.translate(self.w)
+		F = E.translate(-self.v)
+		G = F.translate(self.u)
+		H = G.translate(self.v)
+		
+		ABC = triangle(A,B,C, (250,0,0))
+		ABC.normal = -self.w.normalize()
+		CDA = triangle(C,D,A, (250,250,0))
+		CDA.normal = -self.w.normalize()
+		
+		CBG = triangle(C,B,G, (0,250,0))
+		CBG.normal = self.u.normalize()
+		CGH = triangle(C,G,H, (0,250,250))
+		CGH.normal = self.u.normalize()
+		
+		EGH = triangle(E,G,H, (0, 0, 250))
+		EGH.normal = self.w.normalize()
+		EGF = triangle(E,G,F, (250, 0, 250))
+		EGF.normal = self.w.normalize()
+		
+		EDF = triangle(E,D,F, (250,250,250))
+		EDF.normal = -self.u.normalize()
+		ADF = triangle(A,D,F, (250,125,250))
+		ADF.normal = -self.u.normalize()
+		
+		CHD = triangle(C,H,D, (250,125,150))
+		CHD.normal = self.v.normalize()
+		EDH = triangle(C,D,A, (20,125,250))
+		EDH.normal = self.v.normalize()
+		
+		ABF = triangle(A,B,F, (250,125,0))
+		ABF.normal = -self.v.normalize()
+		BGF = triangle(B,G,F, (100,125,250))
+		BGF.normal = -self.v.normalize()
+		
+		self.triangles = [ABC,CDA,CBG,CGH,EGH,EGF,EDF,ADF,CHD,EDH,ABF,BGF]
+		
+		objects.append(self)
+
+class tri:
+	def __init__(self, A,B,C, colour):
+		self.triangles = [triangle(A,B,C, colour)]
+		#print(self.triangles[0].normal.components())
+		objects.append(self)
+
+
+"""
+class room(box):
+	self.__init__(self, position, u, v, w):
+		super().__init__(position, u, v, w)
+		
+		for tr in self.triangles:
+			tr.normal = -tr.normal
+"""
+
+
+
+box = box(vertex(0,20,0), vector(10,1,0),vector(0,10,0),vector(1,0,10))
+#tri = tri(vertex(-1,1,0),vertex(1,1,0),vertex(0,1,1), (110,110,0))
+camera = camera(vertex(0,0,0), vector(0,1,0), 120, (480,480), vector(1,0,0))
+pixels = camera.cast_rays()
+array = np.array(pixels, dtype=np.uint8)
+
+#print(start, end)
+#print((vector(1,2,3)+vector(1,3,2)).components())
+new_image = Image.fromarray(array)
+new_image.save('new.png')
---
+++ primitives.py
@@ -0,0 +1,215 @@
+import math
+
+def dot(u, v):
+	return u.a * v.a + u.b * v.b + u.c * v.c
+
+def cross(u, v):
+	a = u.b * v.c - u.c * v.b
+	b = u.c * v.a - u.a * v.c
+	c = u.a * v.b - u.b * v.a
+	return a,b,c
+
+def magnitude(a,b,c):
+	return (a**2 + b**2 + c**2)**.5
+
+def normalize(a,b,c):
+	mag = magnitude(a,b,c)
+	return a/mag, b/mag, c/mag
+	
+def triangle_area(A,B,C):
+	abc = cross(vector_2p(B,A), vector_2p(C,A))
+	return magnitude(abc[0], abc[1], abc[2])/2
+
+class vertex:
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+	
+	def coordinates(self):
+		return self.x, self.y, self.z
+	
+	def translate(self, v):
+		return vertex(self.x+v.a, self.y+v.b, self.z+v.c)
+
+global origin 
+origin = vertex(0,0,0)
+
+class matrix:
+	def __init__(self, matrix):
+		self.matrix = matrix
+		self.dimension = self.dimension()
+	
+	def dimension(self):
+		row_length = len(self.matrix[0])
+		for i in self.matrix:
+			if len(i)!= row_length:
+				raise Exception("Not a matrix, row length not equal")
+		return len(self.matrix), row_length
+	
+	def __mul__(self, obj):
+		if isinstance(obj, (int,float)):
+			result = self.matrix
+			for row in result:
+				for i in row:
+					i *= obj
+		elif isinstance(obj, vector) and self.dimension[0] == 3:
+			result = [0,0,0]
+			i = 0
+			for row in self.matrix:
+				result[i] = obj.a * row[0] + obj.b * row[1] + obj.c * row[2]
+				i+=1
+			result = vector(result[0], result[1], result[2])
+		elif isinstance(obj, matrix):
+			#print("need to implement, teehee")
+			pass
+			#todo
+		return result
+
+
+class vector:
+	def __init__(self, a, b, c):
+		self.a = a
+		self.b = b
+		self.c = c
+	
+	def components(self):
+		return self.a, self.b, self.c
+	
+	def magnitude(self):
+		self.magnitude = magnitude(self.a, self.b, self.c)
+		return self.magnitude
+	
+	def normalize(self):
+		a, b, c = normalize(self.a, self.b, self.c)
+		return vector(a,b,c)
+	
+	def cross(self, v):
+		a,b,c = cross(self, v)
+		return vector(a,b,c)
+	
+	def dot(self, v):
+		return dot(self, v)
+	
+	def __add__(self, v):
+		return vector(self.a+v.a, self.b+v.b, self.c+v.c)
+	
+	def __sub__(self, v):
+		v = -v
+		return(self.__add__(v))
+	
+	def __mul__(self, l):
+		return vector(self.a*l, self.b*l, self.c*l)
+	
+	def __neg__(self):
+		return vector(-self.a, -self.b, -self.c)
+	
+	#remember, all rotations are anti-clockwise
+	def rotate_yaw(self, angle):
+		return vector(
+			cos(angle)*self.a - sin(angle)*self.b,
+			sin(angle)*self.a + cos(angle)*self.b,
+			self.c
+			)
+	
+	def rotate_pitch(self, angle):
+		return vector(
+			cos(angle)*self.a  + sin(angle)*self.c,
+			self.b,
+			-sin(angle)*self.a + cos(angle)*self.c
+			)
+
+class vector_2p(vector):
+	def __init__(self, A, B):
+		self.a = A.x - B.x
+		self.b = A.y - B.y
+		self.c = A.z - B.z
+
+class triangle:
+	def __init__(self, A, B, C, colour):
+		self.A = A
+		self.B = B
+		self.C = C
+		self.normal()
+		self.colour = colour
+	
+	def points(self):
+		return self.A, self.B, self.C
+	
+	def normal(self):
+		u = vector_2p(self.A,self.B)
+		v = vector_2p(self.A,self.C)
+		cross_product = u.cross(v)
+		self.area = cross_product.magnitude()/2 
+		self.normal = cross_product.normalize()
+	
+	def get_colour(self, **kwargs):
+		return self.colour
+
+class ray:
+	def __init__(self, point, vector):
+		self.point = point
+		self.ray_vector = vector
+	
+	def intersection(self, triangle):
+		if dot(self.ray_vector, triangle.normal) >= 0:
+			#print("facing different directions")
+			##print(self.ray_vector.components(), triangle.normal.components(), dot(self.ray_vector, triangle.normal))
+			return False
+		if True:
+			#plane-ray intersection
+			D = triangle.normal.dot(vector_2p(origin,triangle.A))
+			t = -(triangle.normal.dot(vector_2p(origin,self.point)) + D)/triangle.normal.dot(self.ray_vector)
+			##print("t=",t)
+			if t <= 0:
+				#print("behind camera")
+				return False
+				
+			plane_intersection = origin.translate(self.ray_vector * t)
+			#print("plane_intersection: ", plane_intersection.coordinates())
+			
+			##print("__________")
+			AB = vector_2p(triangle.A, triangle.B)
+			##print(AB.components())
+			AP = vector_2p(triangle.A, plane_intersection)
+			##print(AP.components())
+			##print("(ABxAP).N = ")
+			if triangle.normal.dot(AB.cross(AP)) <= 0:
+				#print("not left AB")
+				return False
+			
+			BC = vector_2p(triangle.B, triangle.C)
+			BP = vector_2p(triangle.B, plane_intersection)
+			if triangle.normal.dot(BC.cross(BP)) <= 0:
+				#print("not left BC")
+				return False
+			
+			CA = vector_2p(triangle.C, triangle.A)
+			CP = vector_2p(triangle.C, plane_intersection)
+			if triangle.normal.dot(CA.cross(CP)) <= 0:
+				#print("not left CA")
+				return False
+			
+			else:
+				#print("yes")
+				return t
+			
+			"""
+			#barycentric coordinates
+			APC = triangle_area(triangle.A, triangle.C, plane_intersection)
+			if APC > triangle.area:
+				return False
+				
+			else:
+				APB = triangle_area(triangle.A, triangle.B, plane_intersection)
+				if APB > triangle.area:
+					return False
+					
+				elif APB + APC - triangle.area < 0:
+					return False
+					
+				else:
+					return t  #idr what this does , vector(APB, APC, triangle.area - APB + APC) * triangle_area**-1	
+			"""
+			
+			
